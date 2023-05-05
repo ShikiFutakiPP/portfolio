4e9c90ef7c1dbc30712ee56b970ca70630592849
shiki futaki pp <100916708+ShikiFutakiPP@users.noreply.github.com>
2023-05-05 14:52:28 +0200

    formations

--- index.php
+++ index.php
@@ -35,9 +35,8 @@
         <section id="mes-projets">
 
             <?php
-
                 // chemin d'accès à votre fichier JSON
-                $file = 'projects.json'; 
+                $file = 'datas/projects.json'; 
                 // mettre le contenu du fichier dans une variable
                 $data = file_get_contents($file); 
                 // décoder le flux JSON
@@ -53,19 +52,33 @@
                                 echo '</div>
                             </div>';
                 }
-
-
-
-
             ?>
 
         </section>
-        <section id="formation">
-            <h2>Formation</h2>
-            <p>
-                Nulla dignissim consectetur velit sed arcu non sodales. Sed nec arcu tortor.
-                Aenean volutpat dolor eu pretium convallis. Donec hendrerit arcu ac odio.
-            </p>
+
+        <h2>Mes formations</h2>
+
+        <section id="mes-form">
+            <?php
+                // chemin d'accès à votre fichier JSON
+                $fileFormation = 'datas/formations.json'; 
+                // mettre le contenu du fichier dans une variable
+                $dataFormation = file_get_contents($fileFormation); 
+                // décoder le flux JSON
+                $formations = array_reverse(json_decode($dataFormation)); 
+
+                foreach ($formations as $aFormation) {
+
+                    echo    '<div class="card" onClick="document.location.href=\''.$aFormation->link.'\'">
+                                <img src="img/'.$aFormation->img.'" alt="Image de la formation">
+                                <div class="container">
+                                    <h3><b>'.$aFormation->name.'</b></h3>'; 
+                                    //echo '<p>'.$aFormation->description.'</p>';
+                                echo '</div>
+                            </div>';
+                }
+            ?>
+
         </section>
     </main>
 </body
