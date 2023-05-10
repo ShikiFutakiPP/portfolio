3d18a6186cfa57ab96306733fcacba2d6f219203
shiki futaki pp <100916708+ShikiFutakiPP@users.noreply.github.com>
2023-05-10 13:28:58 +0200

    scroll

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
         <nav>
 
             <ul id="navbar">
-                <a href="index.php"><li><img src="portfolio_logo.png" style="width: 20vw;"></li></a>
+                <a href="index.php#my-presentation"><li><img src="portfolio_logo.png" style="width: 20vw;"></li></a>
                 <a href="index.php#my-experience"><li>Expérience</li></a>
                 <a href="index.php#my-projects"><li>Mes Projets</li></a>
                 <a href="index.php#formations"><li>Formation</li></a>
