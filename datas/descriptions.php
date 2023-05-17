d7574e4cc6e03b3f645e8ee05c1849c676ff1e04
shiki futaki pp <100916708+ShikiFutakiPP@users.noreply.github.com>
2023-05-17 13:55:41 +0200

    Update descriptions.php

--- datas/descriptions.php
+++ datas/descriptions.php
@@ -3,10 +3,10 @@ $descriptions = [];
 
 /* PROJETS */
 
-$descriptions['mairie'] = "<b>Cadre :</b> Stage de seconde année de BTS.<br/>
-<b>Destinataires :</b> Agents d'astreinte au sein de la mairie de Fleury-Les-Aubrais.<br/>
+$descriptions['mairie'] = "<b>Modalité :</b> Effectué seul<br/>
+<b>Cadre :</b> Stage de seconde année<br/>
 <br/>
-Dans le cadre de ce stage, j'ai développé un site web permettant aux agents qui étaient d'astreinte : 
+Dans le cadre de ce stage, j'ai développé un site web permettant aux agents de la mairie de Fleury-les-Aubrais qui étaient d'astreinte : 
 <ul>
     <li>d’accéder à des documents (des procédures, des plans,…)</li>
     <li>d'envoyer un rapport de leur intervention</li>
@@ -19,13 +19,29 @@ Dans le cadre de ce stage, j'ai développé un site web permettant aux agents qu
             </a>";
 
 
-$descriptions['lafleur'] = "<b>Cadre :</b> Seconde année de BTS.<br/>";
+$descriptions['lafleur'] = "<b>Modalité :</b> Effectué en équipe<br/>
+<b>Cadre :</b> Projet de seconde année<br/>
+";
 
 
-$descriptions['stage'] = "";
+$descriptions['stage'] = "<b>Modalité :</b> Effectué en équipe<br/>
+<b>Cadre :</b> Projet de fin de seconde année<br/>
+<br/>
+Dans le cadre de ce projet, mon avons poursuivi le développement d'un site qui avait été commencé par les précédents étudiants de BTS SIO mais qui était non fonctionnel et inachevé.<br/>
+Le site devait permettre à chaque élève principalement :
+<ul>
+    <li>d'ajouter ses demandes de stage</li>
+    <li>d'indiquer l'évolution de ses demandes</li>
+    <li>de transmettre aux professeurs son attestation, sa convention et son évaluation de stage</li>
+    <li>d'accéder à une liste d'entreprises déjà solicitées par le passé pour l'aider dans sa recherche de stage</li>
+</ul>
+Il devait également notamment permettre au professeur d'accéder au suivi des demandes de stage de chaque élève et, en début d'année, de paramétrer les périodes de stage.";
 
 
-$descriptions['bucket'] = "";
+$descriptions['bucket'] = "<b>Modalité :</b> Effectué seul<br/>
+<b>Cadre :</b> Travail pratique<br/>
+Dans le cadre de travaux pratiques, 
+";
 
 
 $descriptions['msl'] = "<b>Cadre :</b> Stage de première année de BTS.<br/><b>Destinataires :</b> Les employés de MSL Circuits<br/><br/>";
